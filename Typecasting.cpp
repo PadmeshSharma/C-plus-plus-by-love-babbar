6abd9b0c614d3dc22f5246b674600e64cb85fd7d
Padmesh Sharma <100578426+PadmeshSharma@users.noreply.github.com>
2023-05-06 19:56:10 +0530

    Add files via upload

---
+++ Typecasting.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a='a';
+    cout<< a << endl;
+
+    char b= 98;
+    cout<<b<<endl;
+    
+    char c= 123456;
+    cout<<c<<endl;
+
+}
