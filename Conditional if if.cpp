003f4cba8d6017b8d96e428405290ce0e97312fb
Padmesh Sharma <100578426+PadmeshSharma@users.noreply.github.com>
2023-05-30 11:21:43 +0530

    Add files via upload

---
+++ Conditional if if.cpp
@@ -0,0 +1,14 @@
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int a,b;
+	cout<<"Enter a and b"<<endl;
+	cin>>a>>b;
+	if(a>b)
+		cout<<a<<" is greater than "<<b;
+	if(b>a)
+		cout<<b<<" is greater than "<<a;
+}
